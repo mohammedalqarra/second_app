135ee5633b981f37487022f072328f2e6bcc0e96
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-05-10 12:51:47 +0300

    init:commit add appBar

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,27 +9,6 @@ class homepage extends StatelessWidget {
         centerTitle:true,
         backgroundColor: Color.fromARGB(255, 14, 198, 161),
       ),
-      drawer: Drawer(
-          elevation: 25,
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: const [
-              UserAccountsDrawerHeader(
-                accountName: Text('Mohammed'),
-                accountEmail: Text('moh@gmail.com'),
-                decoration: BoxDecoration(color: Colors.teal),
-                currentAccountPicture: CircleAvatar(
-                  backgroundColor: Colors.pink,
-                  child: Text("M",
-                      style: TextStyle(
-                        fontSize: 30,
-                        color: Colors.white,
-                      )),
-                ),
-              ),
-            ],
-          ),
-          ),
     );
   }
 }
