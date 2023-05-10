f13fcfba143aada3181fcc936bafd45ab437c250
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-05-10 13:07:28 +0300

    init:commit add Drawer

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -6,30 +6,45 @@ class homepage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Quiz app"),
-        centerTitle:true,
+        centerTitle: true,
         backgroundColor: Color.fromARGB(255, 14, 198, 161),
       ),
       drawer: Drawer(
-          elevation: 25,
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: const [
-              UserAccountsDrawerHeader(
-                accountName: Text('Mohammed'),
-                accountEmail: Text('moh@gmail.com'),
-                decoration: BoxDecoration(color: Colors.teal),
-                currentAccountPicture: CircleAvatar(
-                  backgroundColor: Colors.pink,
-                  child: Text("M",
-                      style: TextStyle(
-                        fontSize: 30,
-                        color: Colors.white,
-                      )),
+        elevation: 25,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const [
+            UserAccountsDrawerHeader(
+              accountName: Text('Mohammed'),
+              accountEmail: Text('moh@gmail.com'),
+              decoration: BoxDecoration(color: Colors.teal),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.pink,
+                child: Text(
+                  "M",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ],
-          ),
-          ),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text('Create Quiz'),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text('Start  Quiz'),
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text('Exit'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
