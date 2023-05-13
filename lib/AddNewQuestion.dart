7210d4ee40b812a02362c2cf51e1981fa77f38df
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-05-13 18:38:44 +0300

    init:commit add focus  AddNewQuestionState

--- lib/AddNewQuestion.dart
+++ lib/AddNewQuestion.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -10,6 +11,16 @@ class AddNewQuestion extends StatefulWidget {
 }
 
 class _AddNewQuestionState extends State<AddNewQuestion> {
+  FocusNode focusNode = FocusNode();
+  bool hasFocus = false;
+
+  @override
+  void dispose() {
+    focusNode.dispose();
+
+    super.dispose();
+  }
+
   var selectedItem = "A";
 
   @override
@@ -23,6 +34,36 @@ class _AddNewQuestionState extends State<AddNewQuestion> {
           onPressed: () {},
         ),
         centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Column(
+            children: [
+              TextField(
+                autofocus: true,
+                focusNode: focusNode,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  prefixIcon: const Icon(Icons.question_mark),
+                  labelText: 'Question',
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                  textStyle: const TextStyle(fontSize: 18),
+                  backgroundColor: Colors.teal,
+                ),
+                onPressed: () => focusNode.requestFocus(),
+                child: const Text('Add Question'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
