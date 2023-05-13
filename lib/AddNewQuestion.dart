9112c6a22795ed81299fa93c572327b5f370058c
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-05-14 00:51:54 +0300

    init:commit add items  AddNewQuestionState

--- lib/AddNewQuestion.dart
+++ lib/AddNewQuestion.dart
@@ -14,14 +14,15 @@ class _AddNewQuestionState extends State<AddNewQuestion> {
   FocusNode focusNode = FocusNode();
   bool hasFocus = false;
 
+  List<String> items = <String>['A', 'B', 'C', 'D'];
+  String dropdownValue = "A";
+
   @override
   void dispose() {
     focusNode.dispose();
 
     super.dispose();
   }
-
-  var selectedItem = "A";
 
   @override
   Widget build(BuildContext context) {
@@ -45,11 +46,13 @@ class _AddNewQuestionState extends State<AddNewQuestion> {
                 autofocus: true,
                 focusNode: focusNode,
                 decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.question_mark),
                   labelText: "Question",
                   //   fillColor: Colors.red,
                   border: OutlineInputBorder(
                     //  borderSide:
                     //    const BorderSide(color: Colors.teal, width: 3.0),
+
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
@@ -162,7 +165,6 @@ class _AddNewQuestionState extends State<AddNewQuestion> {
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 20,
-                          
                         ),
                       ),
                     ),
@@ -183,15 +185,73 @@ class _AddNewQuestionState extends State<AddNewQuestion> {
                   ],
                 ),
               ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 230,
+                      child: const Text(
+                        "Select the correct answer",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      //  width: 150,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      child: DropdownButton<String>(
+                        elevation: 50,
+                        iconSize: 20,
+                        icon: Icon(Icons.arrow_drop_down, color: Colors.black),
+                        // hint: Text(
+                        //   'A',
+                        //   style: TextStyle(
+                        //     fontSize: 10,
+                        //     color: Theme.of(context).hintColor,
+                        //   ),
+                        // ),
+                        items: items.map((String value) {
+                          Color itemColor =
+                              Colors.teal; // Specify your desired color here
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(
+                              value,
+                              style: TextStyle(color: itemColor),
+                            ),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            dropdownValue = newValue!;
+                          });
+                        },
+                        value: dropdownValue,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               const SizedBox(height: 20),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   minimumSize: const Size.fromHeight(50),
-                  textStyle: const TextStyle(fontSize: 18),
+                  textStyle: const TextStyle(fontSize: 15),
                   backgroundColor: Colors.teal,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
                 ),
                 onPressed: () => focusNode.requestFocus(),
-                child: const Text('Add Question'),
+                child: const Text('Add question',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+                ),
               ),
             ],
           ),
