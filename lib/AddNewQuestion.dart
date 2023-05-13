74e537d6abdcc06269a2dd865fbf9310f7ef9d50
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-05-13 17:41:36 +0300

    init:commit add AddNewQuestionState&import

--- lib/AddNewQuestion.dart
+++ lib/AddNewQuestion.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
 
-class AddNewQuestion extends StatelessWidget {
+class AddNewQuestion extends StatefulWidget {
+  const AddNewQuestion({
+    super.key
+  });
+
+  @override
+  
+  State<AddNewQuestion> createState() => _AddNewQuestionState();
+
+}
+
+class _AddNewQuestionState extends State<AddNewQuestion> {
   var selectedItem = "A";
 
   @override
