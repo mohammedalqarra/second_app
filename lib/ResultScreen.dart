cdeddff9e47a23e85eab8c9d9955da0d2a16d4b6
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-05-14 13:58:30 +0300

    init:commit add ResultScreen

--- lib/ResultScreen.dart
+++ lib/ResultScreen.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
-class ResultScreen extends StatelessWidget {
+class ResultScreen extends StatefulWidget {
+  final int score;
+
+  ResultScreen({required this.score});
+  @override
+  _ResultScreenState createState() => _ResultScreenState();
+
+}
+
+class _ResultScreenState extends State<ResultScreen> {
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-
-      ),
+      appBar: AppBar(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:second_app/HomePage.dart';
 import 'package:second_app/ResultScreen.dart';
 import 'package:flutter/material.dart';
 //import 'package:second_app/ResultQuiz.dart';
 
-import 'ResultScreen.dart';
+import 'HomePage.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,8 +29,38 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-    // home: ResultQuiz(score: null),
-     home:ResultScreen(),
+      // home: ResultScreen(score: null),
+      home: HomePage(),
     );
   }
 }
+
+
+// class ResultScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Quiz App'),
+//         leading: IconButton(
+//           icon: Icon(Icons.arrow_back),
+//           onPressed: () {},
+//         ),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () {
+//             int quizScore = 80;
+//              Navigator.push(
+//               context,
+//               MaterialPageRoute(
+//                 builder: (context) => ResultScreen(score: quizScore),
+//               ),
+//             );
+//         },
+//         child: Text('Start Quiz'),
+//         ),
+//       ),
+//     );
+//   }
+// }
