e03d884a37433711a4bb34a7fb5a2df3ebb617e3
mohammedalqarra <mohammedalqarra2915@gmail.com>
2023-05-24 22:26:28 +0300

    init:commit add finish project

--- lib/Screens/StartQuiz.dart
+++ lib/Screens/StartQuiz.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'dart:math';
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -41,119 +41,127 @@ class _StartQuizState extends State<StartQuiz> {
           centerTitle: true,
           backgroundColor: Colors.teal,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(width: 393),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
-                Text(
-                  'Question ${currentIndex + 1}',
-                  style: const TextStyle(
-                    color: Colors.teal,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Question ${currentIndex + 1}',
+                      style: const TextStyle(
+                        color: Colors.teal,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '/${databaseProvider.questions.length}',
+                      style: const TextStyle(
+                        color: Colors.blueGrey,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
                 ),
-                Text(
-                  '/${databaseProvider.questions.length}',
-                  style: const TextStyle(
-                    color: Colors.blueGrey,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
+                SizedBox(height: 20),
+                SizedBox(
+                  height: 400,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      controller: scrollController,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: databaseProvider.questions.length,
+                      itemBuilder: (context, int currentIndex) {
+                        return StartQuizWidget(
+                          dataBaseModel: databaseProvider.questions[currentIndex],
+                        );
+                      }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.teal,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        onPressed:
+                            currentIndex == databaseProvider.questions.length - 1
+                                ? () {
+                          print('before condition');
+                                    if (databaseProvider.selectedAnswer ==
+                                        databaseProvider.questions[currentIndex]
+                                            .correctAnswer) {
+                                      print('after condition');
+                                      databaseProvider.score++;
+                                      log(databaseProvider.score.toString());
+                                    }
+                                    databaseProvider.calculateResult();
+                                    Navigator.of(context)
+                                        .pushReplacementNamed('/resultQuiz');
+                                  }
+                                : (){
+                          print('bbbbbbbbbbbbbbb');
+                            },
+                        child: const Text(
+                          'Finished',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.teal,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12))),
+                        onPressed:
+                            currentIndex < databaseProvider.questions.length- 1
+
+                                ? () {
+                          print('beforee');
+                                    if (databaseProvider.selectedAnswer ==
+                                        databaseProvider.questions[currentIndex]
+                                            .correctAnswer) {
+                                      print('afterr');
+                                      databaseProvider.score++;
+                                      log(databaseProvider.score.toString());
+                                    }
+                                    nextItem();
+                          print('after2');
+                                    databaseProvider.changeSelectedAnswer('0');
+                                  }
+                                : null,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: const [
+                            Text(
+                              'Next',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios_rounded,
+                              size: 30,
+                              color: Colors.white,
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
-            SizedBox(height: 20),
-            SizedBox(
-              width: 377,
-              height: 400,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  controller: scrollController,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: databaseProvider.questions.length,
-                  itemBuilder: (context, int currentIndex) {
-                    return StartQuizWidget(
-                      dataBaseModel: databaseProvider.questions[currentIndex],
-                    );
-                  }),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.white,
-                      backgroundColor: Colors.teal,
-                      fixedSize: const Size(120, 40),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                    ),
-                    onPressed:
-                        currentIndex == databaseProvider.questions.length - 1
-                            ? () {
-                                if (databaseProvider.selectedAnswer ==
-                                    databaseProvider.questions[currentIndex]
-                                        .correctAnswer) {
-                                  databaseProvider.score++;
-                                  log(databaseProvider.score.toString() as num);
-                                }
-                                databaseProvider.calculateResult();
-                                Navigator.of(context)
-                                    .pushReplacementNamed('/resultQuiz');
-                              }
-                            : null,
-                    child: const Text(
-                      'Finished',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.teal,
-                        fixedSize: Size(120, 40),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12))),
-                    onPressed:
-                        currentIndex < databaseProvider.questions.length- 1
-                            ? () {
-                                if (databaseProvider.selectedAnswer ==
-                                    databaseProvider.questions[currentIndex]
-                                        .correctAnswer) {
-                                  databaseProvider.score++;
-                                  log(databaseProvider.score.toString() as num);
-                                }
-                                nextItem();
-                                databaseProvider.changeSelectedAnswer('0');
-                              }
-                            : null,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Text(
-                          'Next',
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        Icon(
-                          Icons.arrow_forward_ios_rounded,
-                          size: 30,
-                          color: Colors.white,
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       );
     });
